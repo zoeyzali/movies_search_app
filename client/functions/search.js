ef2dde0ce0c04eae97fde51c298ed2fd6672840f
zoeyzali <zoeyzali@gmail.com>
2020-05-18 23:10:17 +0200

    commits galore, searchfunctionality

--- client/functions/search.js
+++ client/functions/search.js
@@ -7,6 +7,7 @@ const baseURL = process.env.REACT_APP_BASE_URL
 // serach functionality *functions
 exports.handler = async ( event, context, callback ) => {
     const query = event.queryStringParameters.query || ""
+    const controller = new AbortController()
     return fetch( `${baseURL}/search/movie?language=en-US&page=1&include_adult=false&api_key=${tmdbKey}&query=${query}`, { signal: controller.signal } )
         .then( json => {
             console.log( json, "jsonQuery lambda" )
