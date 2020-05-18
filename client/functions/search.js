b04495986eea10a8d05a468c84b65162c47de97f
zoeyzali <zoeyzali@gmail.com>
2020-05-18 23:07:02 +0200

    commits galore, searchfunctionality

--- client/functions/search.js
+++ client/functions/search.js
@@ -1,18 +1,15 @@
-// exports.handler = async ( event, context ) => {
-//     const subject = event.queryStringParameters.name || "World"
-//     return {
-//         statusCode: 200,
-//         body: `Hello ${subject}`
-//     }
-// }
+const fetch = require( 'node-fetch' )
 
+
+const tmdbKey = process.env.REACT_APP_TMDB_KEY
+const baseURL = process.env.REACT_APP_BASE_URL
 
 // serach functionality *functions
 exports.handler = async ( event, context, callback ) => {
-    const query = event.queryStringParameters.query
+    const query = event.queryStringParameters.query || ""
     return fetch( `${baseURL}/search/movie?language=en-US&page=1&include_adult=false&api_key=${tmdbKey}&query=${query}`, { signal: controller.signal } )
         .then( json => {
-            console.log( json, "json query" )
+            console.log( json, "jsonQuery lambda" )
             callback( null, {
                 statusCode: 200,
                 body: JSON.stringify( json.results )
