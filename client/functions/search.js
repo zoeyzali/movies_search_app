7a859dfa4eaa0ba12e6134c134047905599d7b04
zoeyzali <zoeyzali@gmail.com>
2020-05-18 23:15:40 +0200

    commits galore, searchfunctionality

--- client/functions/search.js
+++ client/functions/search.js
@@ -1,14 +1,13 @@
 const fetch = require( 'node-fetch' )
 
-
 const tmdbKey = process.env.REACT_APP_TMDB_KEY
 const baseURL = process.env.REACT_APP_BASE_URL
 
 // serach functionality *functions
 exports.handler = async ( event, context, callback ) => {
     const query = event.queryStringParameters.query || ""
-    const controller = new AbortController()
-    return fetch( `${baseURL}/search/movie?language=en-US&page=1&include_adult=false&api_key=${tmdbKey}&query=${query}`, { signal: controller.signal } )
+    // const controller = new AbortController()
+    return fetch( `${baseURL}/search/movie?language=en-US&page=1&include_adult=false&api_key=${tmdbKey}&query=${query}` )
         .then( json => {
             console.log( json, "jsonQuery lambda" )
             callback( null, {
