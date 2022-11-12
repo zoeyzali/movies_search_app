9be3926c108cc7ff8f2618fa5dfd98c720fececa
zoeyzali <zoeyzali@gmail.com>
2022-11-12 18:19:00 +0100

    dep updates-and-fixes

--- client/functions/movies.js
+++ client/functions/movies.js
@@ -1,24 +1,25 @@
 const fetch = require( 'node-fetch' )
+// import fetch from 'node-fetch'
 const tmdbKey = process.env.REACT_APP_TMDB_KEY
-const baseURL = process.env.REACT_APP_BASE_URL
+// const baseURL = process.env.REACT_APP_BASE_URL
 const accessToken = process.env.REACT_APP_ACCESS_TOKEN
 
 exports.handler = async ( event, context ) => {
-    return fetch( `https://api.themoviedb.org/4/list/140481?language=en-US&page=1&include_adult=false&api_key=${tmdbKey}`,
-        {
-            method: 'GET',
-            headers: {
-                Authorization: accessToken,
-                'Accept': 'application/json'
-            }
-        } )
-        .then( response => response.json() )
-        .then( data => ( {
-            statusCode: 200,
-            body: JSON.stringify( data )
-        } ) )
-        .catch( error => ( {
-            statusCode: 422,
-            body: String( error )
-        } ) )
+  return fetch( `https://api.themoviedb.org/4/list/140481?language=en-US&page=1&include_adult=false&api_key=${tmdbKey}`,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: accessToken,
+        'Accept': 'application/json'
+      }
+    } )
+    .then( response => response.json() )
+    .then( data => ( {
+      statusCode: 200,
+      body: JSON.stringify( data )
+    } ) )
+    .catch( error => ( {
+      statusCode: 422,
+      body: String( error )
+    } ) )
 }
